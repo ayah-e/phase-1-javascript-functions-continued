4d2e9a4b97323646728261c327a10b19a0d4b323
Ayah Elsais <elsais.ayah@gmail.com>
2023-01-11 11:16:09 -0500

    completed all tests

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun(); // => "This saturday, I want to roller-skate!"
+saturdayFun("ice-skate"); // => "This saturday, I want to ice-skate!"
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+// had to define the mondayWork function before assigning to a variable for it to work
+const fn = function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+fn(); // => "This monday, I will go to the office."
+
+function wrapAdjective(special)  {
+    //want the function to wrap an adjective with *
+    const adjective = "a hard worker";
+    return function(adjective) {
+            return `You are ${special}${adjective}${special}!`;
+    };
+};
+
+wrapAdjective("*")();
+wrapAdjective("||")("a dedicated programmer");
+
